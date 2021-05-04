a93466f7047a8adfc685ca8de50d93c72763507f
PGG106 <giuseppepagano10@gmail.com>
2021-05-04 10:21:05 +0200

    fix dello script di creazione del db

--- db/storage.sql
+++ db/storage.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `comicshop` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `comicshop`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: localhost    Database: comicshop
@@ -409,4 +411,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-23  0:34:51
+-- Dump completed on 2021-05-04 10:19:01
